3ed345c2e86a45c68f9919ef2f3a7608c147f999
Alexander Ryazanov <alex.r@mediasapiens.de>
2021-12-12 17:43:12 +0300

    Prisma Global module

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./modules/users/users.module";
 import { CategoriesModule } from "./modules/categories/categories.module";
+import { PrismaModule } from "./modules/prisma/prisma.module";
 
 @Module({
-  imports: [UsersModule, CategoriesModule],
+  imports: [PrismaModule, UsersModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- be/src/modules/users/users.service.ts
+++ be/src/modules/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
-import { PrismaService } from "../../prisma.service";
+import { PrismaService } from "../prisma/prisma.service";
 import { User } from "@prisma/client";
 import { PrismaException } from "../../exceptions/prismaException";
 import { CreateUserDto } from "./dto/create-user.dto";
