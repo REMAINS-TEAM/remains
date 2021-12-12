96288fa13e97f13b743e8e149e040e29e1d3da5b
Alexander Ryazanov <alex.r@mediasapiens.de>
2021-12-12 22:50:07 +0300

    Helmet install

--- be/src/main.ts
+++ be/src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory, Reflector } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { PrismaService } from "./modules/prisma/prisma.service";
 import { ValidationPipe } from "@nestjs/common";
+import helmet from "helmet";
 import authMiddleware from "./middlewares/auth.middleware";
 import { RolesGuard } from "./guards/roles.guard";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Helmet (safety headers)
+  app.use(helmet());
 
   // Prefix
   app.setGlobalPrefix("api");
