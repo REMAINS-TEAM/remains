e2899302836efd96e79260acf836ca0c0f86cb34
Alexander Ryazanov <alex.r@mediasapiens.de>
2022-07-10 23:31:27 +0300

    migration for code

---
+++ be/prisma/migrations/20220710193434_code_table/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Token` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Token" DROP CONSTRAINT "Token_userId_fkey";
+
+-- DropTable
+DROP TABLE "Token";
+
+-- CreateTable
+CREATE TABLE "Code" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "value" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Code_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Code" ADD CONSTRAINT "Code_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
