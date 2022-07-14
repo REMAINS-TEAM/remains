bf7a84a75fa370c19d587d185d606a5a265a0965
Alexander Ryazanov <alex.r@mediasapiens.de>
2022-07-14 23:19:08 +0300

    add parentCategory to findAll EP

--- be/src/modules/categories/categories.service.ts
+++ be/src/modules/categories/categories.service.ts
@@ -18,27 +18,42 @@ export class CategoriesService {
   async findAll({ parentId }: { parentId?: number }): Promise<Category[]> {
     let countSubCategories;
     let categoriesWithCountSub = [];
-    let categories = await this.prisma.category.findMany({
-      where: {
-        parentId: parentId ? +parentId : undefined,
-      },
-      include: {
-        _count: { select: { items: true } },
-      },
-    });
-    if (categories.length) {
-      for (const category of categories) {
-        countSubCategories = await this.prisma.category.count({
-          where: { parentId: category.id },
+    try {
+      let categories = await this.prisma.category.findMany({
+        where: {
+          parentId: parentId ? +parentId : undefined,
+        },
+        include: {
+          _count: { select: { items: true } },
+        },
+      });
+
+      let parentCategory: { title: string } | null = null;
+      if (categories?.[0].parentId) {
+        parentCategory = await this.prisma.category.findUnique({
+          where: { id: categories[0].parentId },
+          select: { title: true },
         });
-        categoriesWithCountSub.push({ ...category, countSubCategories });
       }
-      categories = categoriesWithCountSub.map((category) => ({
-        ...category,
-        itemsCount: category._count.items,
-      }));
+
+      if (categories.length) {
+        for (const category of categories) {
+          countSubCategories = await this.prisma.category.count({
+            where: { parentId: category.id },
+          });
+
+          categoriesWithCountSub.push({
+            ...category,
+            parentCategory,
+            _count: { ...category._count, subCategories: countSubCategories },
+          });
+        }
+      }
+    } catch (err) {
+      throw new PrismaException(err as Error);
     }
-    return categories;
+
+    return categoriesWithCountSub;
   }
 
   async findOne(id: number): Promise<{ category: Category; tree: Category[] }> {
