9332e1286527c83a99109585746b99a161e6ff54
Alexander Ryazanov <alex.r@mediasapiens.de>
2022-07-20 00:02:27 +0300

    search dropdown init

--- fe/src/components/Search/index.tsx
+++ fe/src/components/Search/index.tsx
@@ -1,29 +1,118 @@
-import React from 'react';
-import { IconButton, InputBase, Paper } from '@mui/material';
-import { Search as SearchIcon } from '@mui/icons-material';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  Divider,
+  IconButton,
+  InputBase,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Paper,
+} from '@mui/material';
+import {
+  AccountTreeOutlined as CategoriesIcon,
+  Close as CloseIcon,
+  Search as SearchIcon,
+  Inventory2Outlined as ProductsIcon,
+  BusinessOutlined as CompaniesIcon,
+} from '@mui/icons-material';
+import * as styles from './styles';
 
 function Search() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const [value, setValue] = useState('');
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const clearInput = () => {
+    setValue('');
+  };
+
+  const showDropdown = () => {
+    if (inputRef?.current && value.length) {
+      setAnchorEl((prev) => (!prev ? inputRef?.current : prev));
+      inputRef.current.focus();
+    }
+  };
+
+  const hideDropdown = () => {
+    setAnchorEl(null);
+  };
+
+  useEffect(() => {
+    if (value.length) {
+      showDropdown();
+    } else {
+      hideDropdown();
+    }
+  }, [value]);
+
   return (
-    <Paper
-      component="form"
-      sx={{
-        p: '1px 4px',
-        display: 'flex',
-        alignItems: 'center',
-        width: 315,
-        height: 36,
-        ml: 1,
-      }}
-    >
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Поиск по категориям и товарам"
-        inputProps={{ 'aria-label': 'search' }}
-      />
-      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-    </Paper>
+    <>
+      <Paper component="form" sx={styles.inputPaper}>
+        <InputBase
+          ref={inputRef}
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Поиск по категориям и товарам"
+          inputProps={{ 'aria-label': 'search' }}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onClick={showDropdown}
+        />
+        <IconButton sx={{ p: '10px' }} aria-label="search" onClick={clearInput}>
+          {!value.length ? <SearchIcon /> : <CloseIcon />}
+        </IconButton>
+      </Paper>
+
+      <Menu
+        autoFocus={false}
+        disableAutoFocus
+        disableAutoFocusItem
+        disableEnforceFocus
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={hideDropdown}
+        onClick={hideDropdown}
+        PaperProps={{ elevation: 0, sx: styles.menuPaper }}
+        transformOrigin={{ horizontal: 'left', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+      >
+        <MenuItem disabled>
+          <ListItemIcon>
+            <CategoriesIcon fontSize="small" />
+          </ListItemIcon>
+          Категории
+        </MenuItem>
+        <Divider />
+        <MenuItem>Категория 1</MenuItem>
+        <MenuItem>Категория 2</MenuItem>
+        <MenuItem>Категория 3</MenuItem>
+        <Divider />
+        <MenuItem disabled>
+          <ListItemIcon>
+            <ProductsIcon fontSize="small" />
+          </ListItemIcon>
+          Товары
+        </MenuItem>
+        <Divider />
+        <MenuItem>Товар 1</MenuItem>
+        <MenuItem>Товар 2</MenuItem>
+        <MenuItem>Товар 3</MenuItem>
+        <Divider />
+        <MenuItem disabled>
+          <ListItemIcon>
+            <CompaniesIcon fontSize="small" />
+          </ListItemIcon>
+          Компании
+        </MenuItem>
+        <Divider />
+        <MenuItem>Компания 1</MenuItem>
+        <MenuItem>Компания 2</MenuItem>
+        <MenuItem>Компания 3</MenuItem>
+      </Menu>
+    </>
   );
 }
 
---
+++ fe/src/components/Search/styles.ts
@@ -0,0 +1,20 @@
+import { SxProps } from '@mui/system';
+
+export const inputPaper: SxProps = {
+  p: '1px 4px',
+  display: 'flex',
+  alignItems: 'center',
+  width: 315,
+  height: 36,
+  ml: 1,
+};
+
+export const menuPaper: SxProps = {
+  minWidth: 300,
+  height: 320,
+  overflowX: 'hidden',
+  overflowY: 'auto',
+  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+  background: 'rgba(255,255,255,0.95)',
+  mt: 1.5,
+};
