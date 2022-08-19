5b329f051119fa0dc45791e660a14a601c83e399
Alexander Ryazanov <alex.r@mediasapiens.de>
2022-08-20 02:09:54 +0300

    use imagemin-moz-jpg

--- be/src/modules/storage/storage.service.ts
+++ be/src/modules/storage/storage.service.ts
@@ -1,11 +1,20 @@
 import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { StorageProvider } from './storage.types';
+import imagemin from 'imagemin';
+import sharp from 'sharp';
+import imageminMozJpeg from 'imagemin-mozjpeg';
+import isJpg from 'is-jpg';
 
 @Injectable()
 export class StorageService {
   constructor(
     @Inject('STORAGE_PROVIDER') private storageProvider: StorageProvider,
   ) {}
+
+  async convertToJpeg(buffer: Buffer) {
+    if (isJpg(buffer)) return buffer;
+    return sharp(buffer).jpeg().toBuffer();
+  }
 
   async download(path: string) {
     try {
@@ -17,8 +26,13 @@ export class StorageService {
 
   async upload(path: string, buffer: Buffer) {
     try {
-      return this.storageProvider.upload(path, buffer);
+      const compressedBuffer = await imagemin.buffer(buffer, {
+        plugins: [this.convertToJpeg, imageminMozJpeg({ quality: 75 })],
+      });
+
+      return this.storageProvider.upload(path, compressedBuffer);
     } catch (e) {
+      console.log('e', e);
       throw new BadRequestException('Something went wrong when upload file');
     }
   }
