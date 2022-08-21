6d2024a3de60bfaeba74ceb19c3efea694bc973d
Alexander Ryazanov <alex.r@mediasapiens.de>
2022-08-22 00:26:08 +0300

    cancel fix for mobile viewport

--- fe/src/global/constants.ts
+++ fe/src/global/constants.ts
@@ -1,5 +1,4 @@
 export const APP_HEADER_HEIGHT = 64;
 export const APP_FOOTER_HEIGHT = 48;
-export const VIEWPORT_HEIGHT = 'var(--vh, 1vh) * 100';
 export const LS_KEY_TOKEN = 'remains_token';
 export const LS_KEY_DEMO = 'remains_try_demo';
--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// var for mobile browsers
-document.documentElement.style.setProperty(
-  '--vh',
-  window.innerHeight * 0.01 + 'px',
-);
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
