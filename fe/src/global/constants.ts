121f80263ee5858eb2ded7258acd0d59a439a8b7
Alexander Ryazanov <alex.r@mediasapiens.de>
2022-08-22 00:33:17 +0300

    another footer height for mobile

--- fe/src/global/constants.ts
+++ fe/src/global/constants.ts
@@ -1,4 +1,5 @@
 export const APP_HEADER_HEIGHT = 64;
 export const APP_FOOTER_HEIGHT = 48;
+export const APP_FOOTER_MOBILE_HEIGHT = 150;
 export const LS_KEY_TOKEN = 'remains_token';
 export const LS_KEY_DEMO = 'remains_try_demo';
--- fe/src/layouts/AppLayout/styles.ts
+++ fe/src/layouts/AppLayout/styles.ts
@@ -1,9 +1,17 @@
-import { SxProps } from '@mui/system';
-import { APP_FOOTER_HEIGHT, APP_HEADER_HEIGHT } from 'global/constants';
+import {
+  APP_FOOTER_HEIGHT,
+  APP_FOOTER_MOBILE_HEIGHT,
+  APP_HEADER_HEIGHT,
+} from 'global/constants';
+import { Theme } from '@mui/material';
 
-export const contentContainer: SxProps = {
+export const contentContainer = (theme: Theme) => ({
   position: 'relative',
   top: APP_HEADER_HEIGHT,
   height: `calc(100vh  - ${APP_HEADER_HEIGHT}px - ${APP_FOOTER_HEIGHT}px)`,
   overflowY: 'auto',
-};
+
+  [theme.breakpoints.down('md')]: {
+    height: `calc(100vh  - ${APP_HEADER_HEIGHT}px - ${APP_FOOTER_MOBILE_HEIGHT}px)`,
+  },
+});
