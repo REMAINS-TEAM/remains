a103b6a5300465f5cd0979bd127bda3d949e315b
Alexander Ryazanov <alex.r@mediasapiens.de>
2022-08-26 17:29:02 +0300

    User items show fully

--- be/src/decorators/pagination.decorator.ts
+++ be/src/decorators/pagination.decorator.ts
@@ -8,10 +8,15 @@ import { MAX_ITEMS_FOR_NOT_REGISTERED_USER } from 'constants/main';
 export const Pagination = createParamDecorator(
   async (data: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+    const userIdFilter = request.query.userId;
+
     const isPaid = request.user?.isPaid || false;
 
     const limit = Number(request.query.limit || 5);
     const offset = Number(request.query.offset || 0);
+
+    // User items show fully
+    if (userIdFilter === request.user?.id) return { limit, offset };
 
     let availableLimit = limit;
     if (!isPaid) {
